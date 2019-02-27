ff01b050d3ff30a2c09fe03b58bdf81ebaa413ae
tomascarreras1000 <45202069+tomascarreras1000@users.noreply.github.com>
2019-02-27 12:01:43 +0100

    Properties modified

--- Fatal Fury/main.cpp
+++ Fatal Fury/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include "SDL/include/SDL.h"
-#pragma comment (lib, "SDL2.lib")
-#pragma comment (lib, "SDL2main.lib")
+#pragma comment (lib, "SDL/lib/SDL2.lib")
+#pragma comment (lib, "SDL/lib/SDL2main.lib")
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::cout << "Hello world" << std::endl;
 	system("pause");
 	return 0;
 }
