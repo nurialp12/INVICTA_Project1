5c05859b210d6c361f7b16c8413e6f7c90703ebf
tomascarreras1000 <45202069+tomascarreras1000@users.noreply.github.com>
2019-02-27 11:45:38 +0100

    Include SDL

--- Fatal Fury/main.cpp
+++ Fatal Fury/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include "SDL/include/SDL.h"
+#pragma comment (lib, "SDL2.lib")
+#pragma comment (lib, "SDL2main.lib")
+
 int main()
 {
 	std::cout << "Hello world" << std::endl;
