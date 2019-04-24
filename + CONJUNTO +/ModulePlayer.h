7dcf7268e4bafdc66b5b228b943fcf9ef26447b0
EnricGDV <45202083+EnricGDV@users.noreply.github.com>
2019-04-24 14:42:00 +0200

    State machine

--- + CONJUNTO +/ModulePlayer.h
+++ + CONJUNTO +/ModulePlayer.h
@@ -5,6 +5,10 @@
 #include "Animation.h"
 #include "Globals.h"
 #include "p2Point.h"
+#include "p2Qeue.h"
+
+#define JUMP_TIME 3000
+#define PUNCH_TIME 1000
 
 struct SDL_Texture;
 
@@ -39,7 +43,46 @@ public:
 	bool gmode = false;
 	bool destroyed = false;
 
+	enum terry_states
+	{
+		ST_UNKNOWN,
 
+		ST_IDLE,
+		ST_WALK_FORWARD,
+		ST_WALK_BACKWARD,
+		ST_JUMP_NEUTRAL,
+		ST_JUMP_FORWARD,
+		ST_JUMP_BACKWARD,
+		ST_CROUCH,
+		ST_PUNCH_STANDING,
+		ST_PUNCH_NEUTRAL_JUMP,
+		ST_PUNCH_FORWARD_JUMP,
+		ST_PUNCH_BACKWARD_JUMP,
+		ST_PUNCH_CROUCH
+	};
+
+	enum terry_inputs
+	{
+		IN_LEFT_DOWN,
+		IN_LEFT_UP,
+		IN_RIGHT_DOWN,
+		IN_RIGHT_UP,
+		IN_LEFT_AND_RIGHT,
+		IN_JUMP,
+		IN_CROUCH_UP,
+		IN_CROUCH_DOWN,
+		IN_JUMP_AND_CROUCH,
+		IN_X,
+		IN_JUMP_FINISH,
+		IN_PUNCH_FINISH
+	};
+
+	Uint32 jump_timer = 0;
+	Uint32 punch_timer = 0;
+
+	bool external_input(p2Qeue<terry_inputs>& inputs);
+	void internal_input(p2Qeue<terry_inputs>& inputs);
+	terry_states process_fsm(p2Qeue<terry_inputs>& inputs);
 };
 
 #endif
