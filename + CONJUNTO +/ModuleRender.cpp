1913c2a6d0fe57c4787d33641f4a560ffc7b8aa1
AlexLA99 <45202060+AlexLA99@users.noreply.github.com>
2019-04-26 20:26:39 +0200

    change camera controls

--- + CONJUNTO +/ModuleRender.cpp
+++ + CONJUNTO +/ModuleRender.cpp
@@ -53,10 +53,10 @@ update_status ModuleRender::Update()
 {
 	int speed = 1;
 
-	if(App->input->keyboard[SDL_SCANCODE_UP] == KEY_STATE::KEY_REPEAT)
+	if(App->input->keyboard[SDL_SCANCODE_COMMA] == KEY_STATE::KEY_REPEAT)
 		camera.y += speed;
 
-	if(App->input->keyboard[SDL_SCANCODE_DOWN] == KEY_STATE::KEY_REPEAT)
+	if(App->input->keyboard[SDL_SCANCODE_PERIOD] == KEY_STATE::KEY_REPEAT)
 		camera.y -= speed;
 	
 	if(App->input->keyboard[SDL_SCANCODE_A] == KEY_STATE::KEY_REPEAT )
