c15de6fad94d0ea1dd04726a4265e2a1a02c8117
AlexLA99 <45202060+AlexLA99@users.noreply.github.com>
2019-06-11 01:36:12 +0200

    vídeos

--- + CONJUNTO +/Globals.h
+++ + CONJUNTO +/Globals.h
@@ -28,7 +28,7 @@ typedef unsigned int uint;
 #define SCREEN_SIZE 2
 #define SCREEN_WIDTH 290
 #define SCREEN_HEIGHT 216
-#define WIN_FULLSCREEN 0
+#define WIN_FULLSCREEN 1
 #define WIN_FULLSCREEN_DESKTOP 0
 #define WIN_BORDERLESS 0
 #define WIN_RESIZABLE 0
